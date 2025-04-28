43a3fa795223d643432b902796880fbdc93c5b0a
Guilherme Cardoso <gcardoso@duck.com>
2025-04-28 20:22:17 +1200

    Implement Click-To-Move

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ae33b8f76e10ab2a909608423685a58eb661553c9c2e956102d3aa083677669
-size 528
+oid sha256:873609fa7b2ca2d84f04e9ebd6388ce47e8c5604024ec4785c195a6fda692af6
+size 613
