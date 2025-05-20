6c23eee9649c0e22620eda23530b09de57d1e0a6
Guilherme Cardoso <gcardoso@duck.com>
2025-05-20 22:05:06 +1200

    Remove LFS tracking from Assets/Scripts

--- Assets/Scripts/Core/FollowCamera.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ac8b90f4bae141346655ee6f986bbda3a0e05477c3bc620a8607ccb776a4b2ac
-size 193
--- Assets/Scripts/Movement/Mover.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:45bb9768d2615ffd280566604163b0aed6c2c5cafdd8313c0fb832752af67d9e
-size 693
