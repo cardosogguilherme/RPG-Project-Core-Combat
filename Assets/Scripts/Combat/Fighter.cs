9f9f7f5c01575ae3dc6529cdf300c8d382608028
Guilherme Cardoso <gcardoso@duck.com>
2025-05-28 21:13:12 +1200

    Trigger Animation In Code

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5766dc06846d1903a9bb9ae8fb24a7206547ecdb21578377aeb73b9a954d4e7f
-size 984
+oid sha256:2ddf932f895d9f7caa4338a19442ca60ce3b75bf6d34a9d87fb92d9b7a597f8a
+size 1216
