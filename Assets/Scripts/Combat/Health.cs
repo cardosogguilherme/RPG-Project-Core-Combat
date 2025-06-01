6036013d5ed8061821decf074d15bfc448ee5f24
Guilherme Cardoso <gcardoso@duck.com>
2025-06-01 21:01:12 +1200

    Trigger Death Animation

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a90b506fe4fb969593ee5e397393a1b5ce06bf075caa0c9f753b14c0a7a84b0
-size 320
+oid sha256:71fc5c4134eba3ef531f7d705e2337c3dd20f615b2a1e08539add3188206ebba
+size 651
