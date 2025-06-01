a9572e08d31887339711399afacb4ff202bf68ac
Guilherme Cardoso <gcardoso@duck.com>
2025-06-01 21:22:26 +1200

    Stop Attacking Already

--- Assets/Scripts/Combat/CombarTarget.cs
+++ Assets/Scripts/Combat/CombarTarget.cs
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1606e52f877db99f87188e6859685e9537231cdd53ff2022a2fb09bbbf31fc33
-size 111
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    [RequireComponent(typeof(Health))]
+    public class CombarTarget : MonoBehaviour
+    {
+
+    }
+}
